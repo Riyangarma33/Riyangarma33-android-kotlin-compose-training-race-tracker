46d30eac0f7e9f497f30390fb63e8da5bfa539b6
riyangarma33 <mgarma76@gmail.com>
2024-05-10 17:10:53 +0700

    Add: Congratulation Layout Page.

---
+++ app/src/main/java/com/example/racetracker/ui/Congratulation.kt
@@ -0,0 +1,95 @@
+package com.example.racetracker.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.racetracker.R
+import com.example.racetracker.ui.theme.RaceTrackerTheme
+
+@Composable
+fun Congratulation(
+    winner: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.congratulation),
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(dimensionResource(R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_trophy),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(bottom = dimensionResource(R.dimen.padding_medium))
+            )
+            Spacer(
+                modifier = Modifier.size(dimensionResource(R.dimen.padding_medium))
+            )
+            Text(
+                text = stringResource(R.string.winner, winner),
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
+            ) {
+                Spacer(
+                    modifier = Modifier.size(dimensionResource(R.dimen.padding_medium))
+                )
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(stringResource(R.string.back_to_main))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CongratulationPreview() {
+    RaceTrackerTheme {
+        Congratulation(
+            winner = 1,
+            modifier = Modifier
+                .statusBarsPadding()
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .safeDrawingPadding()
+                .padding(horizontal = dimensionResource(R.dimen.padding_medium)),
+        )
+    }
+}
